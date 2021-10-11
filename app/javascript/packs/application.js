e8331b5ceb511ab1b76309e09dc343910884f812
AngelaMCGonzalez <cgangela187@gmail.com>
2021-10-11 13:15:13 -0500

    Prueba

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,6 @@ import "@hotwired/turbo-rails"
 
 // import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
-import '../stylesheets/application.scss'
+import '../stylesheets/aplication.scss' //import tailwindcss
 import "controllers"
 Rails.start()
